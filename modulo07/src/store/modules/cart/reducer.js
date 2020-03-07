ecc22ace43986cba4f7c36df18b16441a4cf055f
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2020-03-07 20:31:25 -0300

    remover produto

--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -4,9 +4,7 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(
-          product => product.id === action.product.id
-        )
+        const productIndex = draft.findIndex(p => p.id === action.product.id)
 
         if (productIndex >= 0) {
           draft[productIndex].amount += 1
@@ -17,6 +15,16 @@ export default function cart(state = [], action) {
           })
         }
       })
+
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id)
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1)
+        }
+      })
+
     default:
       return state
   }
